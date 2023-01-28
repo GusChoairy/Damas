3f26e32e10f1d539be861e800cc99f282ef90995
Augusto <yriaohc@gmail.com>
2023-01-27 23:14:15 -0300

    Movimentação e Atualização de Tabuleiro

--- damas.py
+++ damas.py
@@ -1,61 +1,87 @@
 
-def seleciona_peca(linha_peca,coluna_peca):
-       
-        if vez == "Jog1":
-            if tabuleiro[linha_peca][coluna_peca] == "1" or tabuleiro[linha_peca][coluna_peca] == "11":
-                return True
-            else:
-                print("Oção invalida! \nEscolha Novamente!")
-                return False
-        else:
-            if tabuleiro[linha_peca][coluna_peca] == "2" or tabuleiro[linha_peca][coluna_peca] == "22":
-                return True
-            else:
-                print("Oção invalida! \nEscolha Novamente!")
-                return False
-
-def movimenta(linha,coluna):
-    if vez=="Jog1" and tabuleiro[linha_peca][coluna_peca] == "1":
-        if linha_peca - linha == 1 and coluna_peca - coluna == 1:
-            tabuleiro[linha_peca - 1][coluna_peca - 1] = tabuleiro[linha_peca][coluna_peca]
-            return True
-        elif linha_peca - linha == -1 and coluna_peca - coluna == 1:
-            tabuleiro[linha_peca + 1][coluna_peca - 1] = tabuleiro[linha_peca][coluna_peca]
-            return True
-        elif linha_peca - linha == -1 and coluna_peca - coluna == -1:
-            tabuleiro[linha_peca + 1][coluna_peca + 1] = tabuleiro[linha_peca][coluna_peca]
-            return True
-        elif linha_peca - linha == 1 and coluna_peca - coluna == -1:
-            tabuleiro[linha_peca - 1][coluna_peca + 1] = tabuleiro[linha_peca][coluna_peca]
-            return True
-    elif tabuleiro[linha_peca][coluna_peca] == "2":
-        if linha_peca - linha == 1 and coluna_peca - coluna == 1:
-            tabuleiro[linha_peca - 1][coluna_peca - 1] = tabuleiro[linha_peca][coluna_peca]
-            return True
-        elif linha_peca - linha == -1 and coluna_peca - coluna == 1:
-            tabuleiro[linha_peca + 1][coluna_peca - 1] = tabuleiro[linha_peca][coluna_peca]
-            return True
-        elif linha_peca - linha == -1 and coluna_peca - coluna == -1:
-            tabuleiro[linha_peca + 1][coluna_peca + 1] = tabuleiro[linha_peca][coluna_peca]
-            return True
-        elif linha_peca - linha == 1 and coluna_peca - coluna == -1:
-            tabuleiro[linha_peca - 1][coluna_peca + 1] = tabuleiro[linha_peca][coluna_peca]
-            return True
-
-def movimenta_peca():
-    print("Mover peça para \n")
-    linha = int(input("Linha: "))
-    coluna = int(input("Coluna: "))
-    print()
-
+def seleciona_peca():
     
-    if tabuleiro[linha_peca][coluna_peca] == "1" or tabuleiro[linha_peca][coluna_peca] == "2" and (linha+coluna)%2==0:
-        tabuleiro[linha][coluna] = tabuleiro[linha_peca][coluna_peca]
-        return True
+    if vez == "Jog1" or vez == "Inicio":
+        if tabuleiro[linha_peca][coluna_peca] == "1" or tabuleiro[linha_peca][coluna_peca] == "11":
+            return True
+        else:
+            print("Oção invalida! \nEscolha Novamente!")
+            return False
     else:
-        return False
+        if tabuleiro[linha_peca][coluna_peca] == "2" or tabuleiro[linha_peca][coluna_peca] == "22":
+            return True
+        else:
+            print("Oção invalida! \nEscolha Novamente!")
+            return False
+
+
+def movimenta(linha_peca,coluna_peca):
+    print()
+    global vez
+    if vez =="Jog1" or vez == "Inicio" and tabuleiro[linha_peca][coluna_peca] == "1":
+
+
+        if linha_peca - linha == 1 and coluna_peca - coluna == 1:
+            tabuleiro[linha_peca - 1][coluna_peca - 1] = tabuleiro[linha_peca][coluna_peca]
+            tabuleiro[linha_peca][coluna_peca] = "0"
+            vez ="Jog2"
+
+            return True
+        
+        elif linha_peca - linha == -1 and coluna_peca - coluna == 1:
+            tabuleiro[linha_peca + 1][coluna_peca - 1] = tabuleiro[linha_peca][coluna_peca]
+            tabuleiro[linha_peca][coluna_peca] = "0"
+            vez="Jog2"
+
+            return True
+        
+        elif linha_peca - linha == -1 and coluna_peca - coluna == -1:
+            tabuleiro[linha_peca + 1][coluna_peca + 1] = tabuleiro[linha_peca][coluna_peca]
+            tabuleiro[linha_peca][coluna_peca] = "0"
+            vez="Jog2"
+
+            return True
+        
+        elif linha_peca - linha == 1 and coluna_peca - coluna == -1:
+            tabuleiro[linha_peca - 1][coluna_peca + 1] = tabuleiro[linha_peca][coluna_peca]
+            tabuleiro[linha_peca][coluna_peca] = "0"
+            vez="Jog2"
+            
+            return True
+        
+
+    elif tabuleiro[linha_peca][coluna_peca] == "2":
+
+        if linha_peca - linha == 1 and coluna_peca - coluna == 1:
+            tabuleiro[linha_peca - 1][coluna_peca - 1] = tabuleiro[linha_peca][coluna_peca]
+            tabuleiro[linha_peca][coluna_peca] = "0"
+            vez="Jog1"
+            
+            return True
+        
+        elif linha_peca - linha == -1 and coluna_peca - coluna == 1:
+            tabuleiro[linha_peca + 1][coluna_peca - 1] = tabuleiro[linha_peca][coluna_peca]
+            tabuleiro[linha_peca][coluna_peca] = "0"
+            vez="Jog1"
+            
+            return True
+        
+        elif linha_peca - linha == -1 and coluna_peca - coluna == -1:
+            tabuleiro[linha_peca + 1][coluna_peca + 1] = tabuleiro[linha_peca][coluna_peca]
+            tabuleiro[linha_peca][coluna_peca] = "0"
+            vez="Jog1"
+            
+            return True
+        
+        elif linha_peca - linha == 1 and coluna_peca - coluna == -1:
+            tabuleiro[linha_peca - 1][coluna_peca + 1] = tabuleiro[linha_peca][coluna_peca]
+            tabuleiro[linha_peca][coluna_peca] = "0"
+            vez="Jog1"
+            
+            return True
 
 def estado_inicial():
+    
     for linha in range(0,8):
         for coluna in range(0,8):
         
@@ -68,6 +94,14 @@ def estado_inicial():
             elif tabuleiro[linha][coluna]=="0" and (linha+coluna)%2== 0:
                 print("\033[;40m  \033[m",end="")
         print()
+
+def rodada(vez):
+    if vez == "Jog1":
+        rodada_Brancas+=1
+        return "Jog2"
+    elif vez == "Jog2":
+        rodada_Pretas +=1
+        return "Jog1"
 
 tabuleiro=[
         ["2","0","2","0","2","0","2","0"],
@@ -84,32 +118,37 @@ vez="Jog1"
 pecas_Pretas = []
 pecas_Brancas = []
 
+
 print()
 print("=~"*2,"Damas","~="*2)
 print("\nInicio de jogo\n")
 
 estado_inicial()
 
-print("\nInforme a peça que deseja mover atravez da linha e da coluna\n0")
-linha_peca = int(input("Linha: "))
-coluna_peca = int(input("Coluna: "))
-while seleciona_peca(linha_peca,coluna_peca) == False:
+print("\nInforme a peça que deseja mover atravez da linha e da coluna")
 
-    linha_peca = int(input("\nLinha: "))
-    coluna_peca = int(input("Coluna: "))
+while True:
     print()
-    seleciona_peca(linha_peca,coluna_peca)
-    break
 
-print("Mover para: ")
-linha = int(input("Linha: "))
-coluna = int(input("Coluna: "))
-movimenta(linha,coluna)
+    if vez == "Jog1":
+        print("Jogador 1, Escolha a peça para andar")
+    else:
+        print("Jogador 2, Escolha a peça para andar")
 
-#while testa_movimentacao() == False:
-#    print("Mover peça para: \n")
-#    linha = int(input("Linha: "))
-#    coluna = int(input("Coluna: "))
-#    testa_movimentacao(linha,coluna)
+    print()
+    print("Andar com a peça: \n")
+    linha_peca = int(input("Linha: "))
+    coluna_peca = int(input("Coluna: "))
+
+    seleciona_peca()
+
+    print()
+    print("Mover para: ")
+    linha = int(input("Linha: "))
+    coluna = int(input("Coluna: "))
+    print(vez)    
+    movimenta(linha_peca,coluna_peca)
+    print(vez)
+    estado_inicial()
 
 
